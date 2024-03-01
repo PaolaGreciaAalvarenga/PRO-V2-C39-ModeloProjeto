fd8105299fb37cb0a7683d8fc7cfb8dc8d78082c
PaolaGreciaAalvarenga <paola.g.alvarenga@gmail.com>
2024-03-01 16:45:02 -0300

    update

--- sketch.js
+++ sketch.js
@@ -217,19 +217,8 @@ function spawnObstacles() {
 
 }*/
 
-/*function reset(){
-  gameState = PLAY;
-  gameOver.visible = false;
-  restart.visible = false;
-  kangaroo.visible = false;
-  kangaroo.changeAnimation("running",
-               kangaroo_running);
-  obstaclesGroup.destroyeach();
-  shrubsGroup.destroyeach();
-  score = 0;
-}*/
 
-/*function reset(){
+function reset(){
   gameState = PLAY;
   gameOver.visible = false;
   restart.visible = false;
@@ -239,5 +228,5 @@ function spawnObstacles() {
   obstaclesGroup.destroyEach();
   shrubsGroup.destroyEach();
   score = 0;
-}*/
+}
 
